f3464501f81dd81918797d3ec2b787dbf65be909
mbglab <48616115+mbglab@users.noreply.github.com>
2025-08-30 02:19:27 +0800

    Add files via upload

--- DD/hDD/ts/nr/hDDL_nr_g.py
+++ DD/hDD/ts/nr/hDDL_nr_g.py
@@ -1,9 +1,8 @@
-# Copyright (C) Bin-Guang Ma (mbg@mail.hzau.edu.cn). All rights reserved.
-# The Numeric Dual Descriptor class (Tensor form) with hierarchical structure
-# Modified to support sequence length transformation between layers using Linker matrices
-# Added linker_trainable parameter to control Linker matrix training
-# Added layer normalization and residual connections similar to PyTorch implementation
-# Author: Bin-Guang Ma; Date: 2025-6-4
+# Copyright (C) 2005-2025, Bin-Guang Ma (mbg@mail.hzau.edu.cn); SPDX-License-Identifier: MIT
+# The Hierarchical Numeric Dual Descriptor (Tensor form) with Linker matrices in pure Python
+# Added layer normalization and residual connections and generation capability
+# This program is for the demonstration of methodology and not fully refined.
+# Author: Bin-Guang Ma (assisted by DeepSeek); Date: 2025-6-24
 
 import math
 import random
