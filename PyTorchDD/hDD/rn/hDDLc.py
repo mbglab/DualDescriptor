dbd9f474966dc66c34d149c93f4b887a1911897d
mbglab <48616115+mbglab@users.noreply.github.com>
2025-10-13 03:18:36 +0800

    Add files via upload
    Signed-off-by: mbglab <48616115+mbglab@users.noreply.github.com>

--- PyTorchDD/hDD/rn/hDDLc.py
+++ PyTorchDD/hDD/rn/hDDLc.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2005-2025, Bin-Guang Ma (mbg@mail.hzau.edu.cn); SPDX-License-Identifier: MIT
-# Hierarchical Dual Descriptor (Random AB matrix form) and Character Sequence Input (HierDDLrnC)
+# Hierarchical Dual Descriptor (Random AB matrix form) with Character Sequence Input (HierDDLrnC)
 # Combines character-level processing and hierarchical vector processing with Linker matrices
 # This program is for the demonstration of methodology and not fully refined.
 # Author: Bin-Guang Ma (assisted by DeepSeek); Date: 2025-10-12
